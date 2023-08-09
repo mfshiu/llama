df3f7e3fbfe45bfdd2ff71b97b0b176d0b4d5aa6
你的名字 <avatar.xu@gmail.com>
2023-08-09 11:33:12 +0200

    temperature

--- test_robot_task.py
+++ test_robot_task.py
@@ -11,9 +11,9 @@ from llama import Llama
 def main(
     ckpt_dir: str,
     tokenizer_path: str,
-    temperature: float = 0.6,
+    temperature: float = 0,
     top_p: float = 0.9,
-    max_seq_len: int = 512,
+    max_seq_len: int = 1024,
     max_batch_size: int = 8,
     max_gen_len: Optional[int] = None,
 ):
