cebcfdbf86af1cf2ef59841b1ad5eeff5f8ef655
你的名字 <avatar.xu@gmail.com>
2023-08-09 11:24:15 +0200

    robot task

--- test_robot_task.py
+++ test_robot_task.py
@@ -24,43 +24,55 @@ def main(
         max_batch_size=max_batch_size,
     )
 
+    user_prompt = "I would like to go to the park."
+    delimiter = "####"
     dialogs = [
-        [{"role": "user", "content": "what is the recipe of mayonnaise?"}],
         [
-            {"role": "user", "content": "I am going to Paris, what should I see?"},
-            {
-                "role": "assistant",
-                "content": """\
-Paris, the capital of France, is known for its stunning architecture, art museums, historical landmarks, and romantic atmosphere. Here are some of the top attractions to see in Paris:
-
-1. The Eiffel Tower: The iconic Eiffel Tower is one of the most recognizable landmarks in the world and offers breathtaking views of the city.
-2. The Louvre Museum: The Louvre is one of the world's largest and most famous museums, housing an impressive collection of art and artifacts, including the Mona Lisa.
-3. Notre-Dame Cathedral: This beautiful cathedral is one of the most famous landmarks in Paris and is known for its Gothic architecture and stunning stained glass windows.
-
-These are just a few of the many attractions that Paris has to offer. With so much to see and do, it's no wonder that Paris is one of the most popular tourist destinations in the world.""",
-            },
-            {"role": "user", "content": "What is so great about #1?"},
-        ],
-        [
-            {"role": "system", "content": "Always answer with Haiku"},
-            {"role": "user", "content": "I am going to Paris, what should I see?"},
-        ],
-        [
+            # {"role": "system", "content": "You are a sentence analyzer."},
             {
                 "role": "system",
-                "content": "Always answer with emojis",
-            },
-            {"role": "user", "content": "How to go from Beijing to NY?"},
-        ],
-        [
-            {
-                "role": "system",
-                "content": """\
-You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+                "content": f"""\
+You will receive an instruction from a user.
+The user's directive will be separated by {delimiter} characters.
+Please categorize the instruction into major and minor categories.
+And provide your output in json format with key values: primary (major category) and secondary (minor category).
 
-If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.""",
+Primary (main category): go somewhere, get items, clean up the mess, provide information, greeting or unsupported categories.
+
+minor categories of greeting:
+normal
+happy
+
+minor categories of go somewhere:
+go to a park
+go to a entrance
+go to a toilet
+go to a export
+go to a restaurant
+
+minor categories of get items:
+take a book
+take a glass of water
+take the remote control
+take a fruit
+take some items
+
+minor categories of clean up the mess:
+clear the table
+clean up the ground
+clean windows
+clean others 
+
+minor categories of provide information:
+product specification
+price
+reviews
+restaurant suggestion
+others
+talk to real people
+""",
             },
-            {"role": "user", "content": "Write a brief birthday message to John"},
+            {"role": "user", "content": f"{delimiter}{user_prompt}{delimiter}"},
         ],
     ]
     results = generator.chat_completion(
